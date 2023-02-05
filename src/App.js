97a114f3fe403121ca29b27ed9d2191d9c6a3166
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-02-05 16:13:41 +0000

    Set geolocation and request weather data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      hello
-    </div>
-  );
-}
+const App = () => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [loading, setLoading] = useState("true");
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLoading(true);
+        const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${process.env.REACT_APP_OWEATHER_API_KEY}`;
+
+        fetch(url)
+          .then((res) => res.json())
+          .then((data) => {
+            setWeatherData(data);
+            setLoading(false);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }, []);
+
+  return <div>{console.log(weatherData)}</div>;
+};
 
 export default App;
--- src/components/WeatherContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const WeatherContext = () => {
-  return (
-    <div>WeatherContext</div>
-  )
-}
