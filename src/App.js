e99691ae1da97efea4cc8e9ceefd6aaa7bd9e387
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-02-20 19:24:23 +0000

    Use class-based component in App.js, add basic CSS

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import PageNotFound from "./components/pages/PageNotFound";
+import Weather from "./components/weather/Weather";
 import "./App.css";
 
-const App = () => {
-  const [weatherData, setWeatherData] = useState([]);
-  const [loading, setLoading] = useState("true");
+class App extends React.Component {
+  // const [weatherData, setWeatherData] = useState();
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${process.env.REACT_APP_OWEATHER_API_KEY}`;
+
+  //       fetch(url)
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           setWeatherData(data);
+  //           setLoading(false);
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //         });
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }, []);
+
+  constructor() {
+    super();
+    this.state = {
+      weatherData: [],
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true });
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        setLoading(true);
         const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&exclude=minutely,hourly,daily,alerts&appid=${process.env.REACT_APP_OWEATHER_API_KEY}`;
-
         fetch(url)
           .then((res) => res.json())
           .then((data) => {
-            setWeatherData(data);
-            setLoading(false);
+            this.setState({ weatherData: data });
+            this.setState({ loading: false });
           })
           .catch((err) => {
             console.log(err);
@@ -25,9 +58,26 @@ const App = () => {
         console.log(error);
       }
     );
-  }, []);
+  }
 
-  return <div>{console.log(weatherData)}</div>;
-};
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path='/'
+              element={<Weather weatherData={this.state.weatherData} />}
+            ></Route>
+            <Route exact path='/about' element={<About />}></Route>
+            <Route path='*' element={<PageNotFound />}></Route>
+          </Routes>
+        </Router>
+      </div>
+    );
+  }
+}
 
 export default App;
