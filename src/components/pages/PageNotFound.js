aff637fa0467f1696293ae9cfe0e1c81479ce90f
Zsolt Kaszas <pokerkaszy7@gmail.com>
2023-02-20 19:27:52 +0000

    Create PageNotFound component to handle wrong urls

---
+++ src/components/pages/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div className='container p-1 text-center bg-danger'>
+      <h1>ERROR - 404</h1>
+      <p>The page you looking for does not exist :(</p>
+    </div>
+  )
+}
+
+export default PageNotFound
